a24733093e4459ed997eb0f4a245b75d1a16bcfa
suzan <sznghimire61@gmail.com>
2022-09-24 01:59:08 +0545

    Final

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 let randNumber1 = Math.floor((Math.random() * 6) + 1);
+let randNumber2 = Math.floor((Math.random() * 6) + 1);
 
-let randNumberImg = document.querySelectorAll("img");
+let randNumberImg1 = document.getElementById("img1");
+let randNumberImg2 = document.getElementById("img2");
 
-randNumberImg.setAttribute("src", "images/dice" + randNumber1 + ".png");
+let randTextImg1 = document.getElementById("topic");
+
+randNumberImg1.setAttribute("src", "images/dice" + randNumber1 + ".png");
+randNumberImg2.setAttribute("src", "images/dice" + randNumber2 + ".png");
+
+
+if (randNumber1 > randNumber2) {
+    randTextImg1.innerHTML = "👏🏻Player1 Wins";
+} else {
+    if (randNumber2 > randNumber1) {
+        randTextImg1.innerHTML = "Player2 Wins👏🏻";
+    } else {
+        if (randNumber2 = randNumber1) {
+            randTextImg1.innerHTML = "🙌🏻Draw!🙌🏻";
+        } else {
+
+        }
+    }
+}
 
 
 
-
-
-
-console.log(randNumber1);
-console.log(randNumberImg);
